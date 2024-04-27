5e73bf79ffef4e01e0e7ea50c1dfd3ff14528124
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2024-04-27 15:40:56 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import PackingList from "./assets/components/PackingList";
-
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
 export default function App() {
-  // const person = "Tamim";
-  // const size = 38;
-  // const age = 42;
+  const chemists = people.filter((person) => person.profession === "chemist");
+
+  const listItems = chemists.map((chemist) => (
+    <li key={chemist.id}>{chemist.name}</li>
+  ));
   return (
     <div>
       <div>
-        <PackingList></PackingList>
+        <ul>{listItems}</ul>
       </div>
     </div>
   );
